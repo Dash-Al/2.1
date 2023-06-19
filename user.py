c20ed8c66fd3bf5afbd77ede8485a34947f1eacf
Дарья <125445507+Dash-Al@users.noreply.github.com>
2023-06-19 17:59:14 +0300

    Create user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+name = input ("What your name? ")
+age = input ("How old are you? ")
+live = input ("Where are you live? ")
+print ("This is", name)
+print ("It is", age)
+print ("(S)he lives in", live)
